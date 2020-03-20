8026002e978416264b509b57f05aa0368ad0118a
endoor-2020-010 <endoor-2020-010@outlook.com>
2020-03-20 12:54:59 +0100

    Add project files.

---
+++ MiniBioApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBioApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Emre Dursun";
+            int leeftijd = 32;
+            string geboorteplaats = "Turkije";
+            string vorigBeroep = "wiskundige";
+            string woonplaats = "Haaksbergen";
+            Console.WriteLine("Ik ben " + fullName + ", " + leeftijd + " jaar " + "en geboren in " + geboorteplaats + ".");
+            Console.Write($"Als {vorigBeroep} werkte ik meer dan 3 jaar in verschillende landen.");
+            Console.Write(" ");
+            Console.Write("Ik woon momenteel in " + woonplaats + ".");
+            Console.ReadLine();
+        }
+    }
+}
